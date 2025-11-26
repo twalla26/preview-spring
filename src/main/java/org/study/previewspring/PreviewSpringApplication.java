33d8a0891878cfaadefc414d19464cbdd825f4c1
twalla26 <ssm4748@naver.com>
2025-11-26 16:49:55 +0900

    chore: initialize springboot project

---
+++ src/main/java/org/study/previewspring/PreviewSpringApplication.java
@@ -0,0 +1,13 @@
+package org.study.previewspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviewSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreviewSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/previewspring/PreviewSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.previewspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreviewSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
